05894cb8843bc61274ae1f09f8703a831a3d69b7
Hummer5235 <92242079+Hummer5235@users.noreply.github.com>
2022-07-31 14:09:43 +0300

    Добавление прыжка

--- Shooter.py
+++ Shooter.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -12,38 +13,44 @@ pygame.display.set_caption("Shooter")
 clock = pygame.time.Clock()
 FPS = 60
 
+GRAVITY = 0.75
+
 #Переменные действия игрока
 moving_left = False
 moving_right = False
 
 BG = (144,201,120)
+RED = (255,0,0)
 
 def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen,RED,(0,400),(SCREEN_WIDTH,400),3)
 
 class Soldier(pygame.sprite.Sprite):
     def __init__(self,char_type,x,y,scale,speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
         self.flip = False
         self.animation_list = []
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
-        temp_list = []
-        for i in range(5):
-            img = pygame.image.load(f"img/{self.char_type}/Idle/{i}.png").convert_alpha()
-            img = pygame.transform.scale(img,(img.get_width() * scale,img.get_height()*scale  ))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)
-        temp_list = []
-        for i in range(6):
-            img = pygame.image.load(f"img/{self.char_type}/Run/{i}.png").convert_alpha()
-            img = pygame.transform.scale(img,(img.get_width() * scale,img.get_height()*scale  ))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)
+        animation_types = ["Idle","Run","Jump"]
+        for animation in animation_types:
+            temp_list = []
+            #Найти количество объектов в папке
+            num_of_frames = len(os.listdir(f"img/{self.char_type}/{animation}"))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f"img/{self.char_type}/{animation}/{i}.png").convert_alpha()
+                img = pygame.transform.scale(img,(img.get_width() * scale,img.get_height()*scale  ))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center  = (x,y)
@@ -69,6 +76,7 @@ class Soldier(pygame.sprite.Sprite):
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image,self.flip,False),self.rect)
+        # pygame.draw.rect(screen,RED,(self.rect.x,self.rect.y,self.rect.width,self.rect.height),3)
 
     def move(self,moving_left,moving_right):
         #Обнулить переменные перемещения
@@ -83,6 +91,19 @@ class Soldier(pygame.sprite.Sprite):
             dx = self.speed
             self.direction = 1
             self.flip = False
+
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+
+        if self.rect.bottom + dy > 400:
+            dy = 400 - self.rect.bottom
+            self.in_air = False
 
         #обновление позиции игрока
         self.rect.x += dx
@@ -101,10 +122,13 @@ while run:
     enemy.draw()
 
     # Обновление действий игрока
-    if moving_left or moving_right:
-        player.update_action(1)
-    else:
-        player.update_action(0)
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)
+        elif moving_left or moving_right:
+            player.update_action(1)
+        else:
+            player.update_action(0)
 
     player.move(moving_left,moving_right)
     player.update_animation()
@@ -121,9 +145,12 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive :
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 quit()
+
 
         # keyboard button released
         if event.type == pygame.KEYUP:
